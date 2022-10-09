290d576328f67f8b12796cc3b5e1db7a87839516
Yash Kawle <90922408+yashkawale@users.noreply.github.com>
2022-10-10 02:01:25 +0530

    Add files via upload

---
+++ site.js
@@ -0,0 +1,26 @@
+function dis(val) {
+    document.getElementById("result").value += val;
+}
+function keyevent(event) {
+    if (event.key == '0' || event.key == '1' || event.key == '2' || event.key == '3' ||
+        event.key == '4' || event.key == '5' || event.key == '6' || event.key == '7' || event.key == '8' ||
+        event.key == '9' || event.key == '/' || event.key == '*' || event.key == '-' || event.key == '+')
+        document.getElementById(event).value += event.key;
+}
+function clr() {
+    document.getElementById("result").value = "";
+}
+var cal = document.getElementById("calculator");
+cal.onkeyup = function (event) {
+    if (event.keycode === 13) {
+        console.log = ("Enter");
+        let x = document.getElementById("result").value
+        console.log(x);
+        solve();
+    }
+}
+function solve() {
+    let x = document.getElementById("result").value
+    let y = eval(x)
+    document.getElementById("result").value = y
+}
